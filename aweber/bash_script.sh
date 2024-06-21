39f6ffcfb61322f57a419881f2ece64f249f88c3
dnlpankov <dnlpankov@gmail.com>
2024-06-21 13:47:20 +0300

    update requirements

---
+++ aweber/bash_script.sh
@@ -0,0 +1,9 @@
+# launch it to automaticall send files to github
+
+conda activate aweber
+cd aweber
+pip freeze > requirements.txt
+git add .
+git commit -m "update requirements"
+git push origin master
+
